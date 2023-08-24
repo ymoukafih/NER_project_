4a413d992e9147a90b7f57a14fdd8183e42dacbc
ymoukafih <ymoukafih99@gmail.com>
2023-08-24 19:05:15 +0100

    Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -20,7 +20,7 @@ from seqeval.metrics import classification_report, f1_score, accuracy_score
 from tqdm import tqdm, trange
 from fairseq.models.roberta import XLMRModel
 from TorchCRF import CRF
-from Training import evaluate_model_seq_labeling
+
 
 
 #load best/ saved model
@@ -30,6 +30,72 @@ model.load_state_dict(state_dict)
 print("Loaded saved model")
 
 model.to(device)
+
+
+def evaluate_model_seq_labeling(model, eval_dataset, label_list, batch_size, use_crf, device, pred=False):
+     # Run prediction for full data
+     eval_sampler = SequentialSampler(eval_dataset)
+     eval_dataloader = DataLoader(
+          eval_dataset, sampler=eval_sampler, batch_size=batch_size)
+
+     model.eval() # turn of dropout
+
+     y_true = []
+     y_pred = []
+
+     label_map = {i: label for i, label in enumerate(label_list, 1)}
+     label_map[0] = "IGNORE"
+
+
+     for input_ids, label_ids, l_mask, valid_ids in eval_dataloader:
+
+          input_ids = input_ids.to(device)
+          label_ids = label_ids.to(device)
+
+          valid_ids = valid_ids.to(device)
+          l_mask = l_mask.to(device)
+
+          with torch.no_grad():
+               logits = model(input_ids, labels=None, labels_mask=None,
+                              valid_mask=valid_ids)
+
+          if use_crf:
+               predicted_labels = model.decode_logits(logits, mask=l_mask, device=device)
+          else :
+               predicted_labels = torch.argmax(logits, dim=2)
+
+          predicted_labels = predicted_labels.detach().cpu().numpy()
+          label_ids = label_ids.cpu().numpy()
+
+          for i, cur_label in enumerate(label_ids):
+               temp_1 = []
+               temp_2 = []
+
+               for j, m in enumerate(cur_label):
+                   if valid_ids[i][j] and label_map[m] not in ['WB' , 'TB']: #'PROG_PART', 'NEG_PART']:  # if it's a valid label
+                         temp_1.append(label_map[m])
+                         temp_2.append(label_map[predicted_labels[i][j]])
+
+               assert len(temp_1) == len(temp_2)
+               y_true.append(temp_1)
+               y_pred.append(temp_2)
+
+     report = classification_report(y_true, y_pred, digits=4)
+     f1 = f1_score(y_true, y_pred, average='macro')
+     acc = accuracy_score(y_true, y_pred)
+
+     s = "Accuracy = {}".format(acc)
+     print(s)
+     report +='\n\n'+ s
+
+     if 'NOUN' in label_map.values():
+         print("Returning acc")
+         f1=acc
+
+     if pred:
+         return f1, report, y_true, y_pred
+     return f1, report
+
 
 predict_file = "./low-resource-seq-labeling/data/UNLABELED/unlabeled_aoc/sents.unlabeled"
 pred_examples = data_processor.get_pred_examples(predict_file)
